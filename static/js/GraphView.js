9e863f51e1c9a3f05001439aa0f0a425e70d2ac9
Jon Lebensold <jon@lebensold.ca>
2012-03-29 09:35:19 +0200

    rewrote drawing implementation for branches

--- static/js/GraphView.js
+++ static/js/GraphView.js
@@ -17,41 +17,55 @@ window.GraphView = Backbone.View.extend({
 	drawColumns: function() {
 		var c = this.columns();
 		var width = Math.floor($(this.el).width() / c);
+		$(this.el).append("<div class='columns'></div>");
 		for(var i = 0; i < c; i++) {
-			$(this.el).append("<div class='col_"+(i+1)+"' style='margin:3px;float:left;width:"+(width-8)+"px;height:500px;'></div>");
+			$(this.el).find('.columns').append("<div class='col_"+(i+1)+"' style='float:left;width:"+(width-8)+"px;height:500px;padding:0px;'></div>");
 		}
 
 		this.drawNode(this.model);
 	},
 
 	drawNode: function(node) {
-		$(this.el).find(".col_"+node.depth()).append("<div id='"+node.cid+"' class='"+node.get('type')+"' style='background:#FFF;width:60px;height:60px;border:1px solid #333;'>"+node.get('type')+"</div>");
+		$(this.el).find(".col_"+node.depth()).append("<div id='"+node.cid+"' class='node "+node.get('type')+"' >"+node.get('type')+"</div>");
 		this.drawBranches(node.get('branches'));
 	},
 
 	drawBranches: function(branches) {
 		var ctx = this.canvasContext();
 
-
 		branches.each(function(b) {
 			if (b.get('node') == null)
 		{
-			console.log(b,"undefined node");
 			return;
 		}
 			this.drawNode(b.get('node'));
 
+			// get node dimensions:
+			var bn = $("#"+b.get('backnode').cid);
+			var nodeDim = {
+				w:bn.width(),
+				h:bn.height(),
+				col:b.get('backnode').depth()
+			};
+			var c = this.columns();
+			var colWidth = Math.floor($(this.el).width() / c);
+
+			src = { left: 0 + ( (nodeDim.col - 1) * (colWidth)),
+							top: nodeDim.h / 2 };
+			dst = { left: (nodeDim.col * colWidth),
+							top: nodeDim.h / 2 + (branches.indexOf(b) * nodeDim.h)};
+
+			src.left = src.left + nodeDim.w - 10;
+			dst.left = dst.left - 10;
+
 			ctx.lineWidth = 1;
 			ctx.beginPath();
-			var src = $("#"+b.get('backnode').cid).offset();
-			var dst = $("#"+b.get('node').cid).offset();
-
-			ctx.moveTo(src.left + 43,src.top -60 - 30);
-			ctx.lineTo(dst.left,dst.top -60 - 30);
+			ctx.moveTo(src.left,src.top);
+			ctx.lineTo(dst.left,dst.top);
 			ctx.stroke();
 			
-			var l =  (dst.left - src.left) * (b.get('backnode').depth()) - 120;
-			ctx.fillText(b.get('txt'),l, (src.top + dst.top / 2)- 160);
+			var l =  (dst.left - src.left + nodeDim.w * 2) * (b.get('backnode').depth()- .9);
+			ctx.fillText(b.get('txt'),l, (src.top + dst.top / 2)+5);
 
 
 		},this);
--- static/js/tree.js
+++ static/js/tree.js
@@ -3,212 +3,26 @@ window.AppView = Backbone.View.extend({
 
 	},
 	initialize: function() {
-		_.bindAll(this,'render');
-		this.render();
+		_.bindAll(this,'render','setTree');
+	},
+	container: function() {
+		return "<div style='margin: 0 auto;position:relative;width:900px;height:500px;padding:0' id='cvs'>"+
+			"<canvas style='position: absolute;left:0;top:0;margin:0;padding:0;' width='900' height='500'></canvas>"+
+			"</div>"; 
 	},
 	render: function() {
-    $(this.el).html("<canvas></canvas><h2>foo</h2>");
-	
+    $(this.el).html(this.container());
+		var v = new GraphView({model:this.model,el:$("#cvs") });
+		v.drawColumns();
+	},
+	setTree: function(tree) {
+	 	var nodes = _.map(tree.nodes,function(k) { return new Node(k); },this);
+		var branches = _.map(tree.branches, function(k){ return new Branch(k); },this);
+		this.model = nodes[0].linkByKey(branches,nodes);
 	}
 
 });
 
-window.GraphView = Backbone.View.extend({
-	initialize: function() {
-		_.bindAll(this,'render','columns','drawColumns','drawNode','drawBranches','canvasContext');
-	},
-	
-	render: function() {
-
-	},
-	
-	columns: function() {
-		return this.model.treeDepth();
-	},
-	canvasContext: function() {
-		return $(this.el).find('canvas').get(0).getContext('2d');
-	},
-
-	drawColumns: function() {
-		var c = this.columns();
-		var width = Math.floor($(this.el).width() / c);
-		for(var i = 0; i < c; i++) {
-			$(this.el).append("<div class='col_"+(i+1)+"' style='margin:3px;float:left;width:"+(width-8)+"px;height:500px;'></div>");
-		}
-
-		this.drawNode(this.model);
-	},
-
-	drawNode: function(node) {
-		$(this.el).find(".col_"+node.depth()).append("<div id='"+node.cid+"' class='"+node.get('type')+"' style='background:#FFF;width:60px;height:60px;border:1px solid #333;'>"+node.get('type')+"</div>");
-		this.drawBranches(node.get('branches'));
-	},
-
-	drawBranches: function(branches) {
-		var ctx = this.canvasContext();
 
 
-		branches.each(function(b) {
-			if (b.get('node') == null)
-		{
-			console.log(b,"undefined node");
-			return;
-		}
-			this.drawNode(b.get('node'));
-
-			ctx.lineWidth = 1;
-			ctx.beginPath();
-			var src = $("#"+b.get('backnode').cid).offset();
-			var dst = $("#"+b.get('node').cid).offset();
-
-			ctx.moveTo(src.left + 43,src.top -60 - 30);
-			ctx.lineTo(dst.left,dst.top -60 - 30);
-			ctx.stroke();
-			
-			var l =  (dst.left - src.left) * (b.get('backnode').depth()) - 120;
-			ctx.fillText(b.get('txt'),l, (src.top + dst.top / 2)- 160);
-
-
-		},this);
-	}
-});
-
-window.Branch = Backbone.Model.extend({
-	defaults: function() {
-		return {
-		backnode: null,
-		node: null,
-		txt: "",
-		key: ""
-		};
-	},
-	
-	initialize: function() {
-
-	},
-
-	asJSON:	function() {
-		return {
-			node: (this.get('node') != null) ? this.get('node').asJSON() : null,
-			txt:	this.get('txt')
-		};
-	},
-
-	addNode: function(n) {
-		this.set('node',new Node(n));
-		this.get('node').set('backbranch',this);
-	},
-
-	linkByKey: function(branches, nodes) {
-		_.each(nodes,function(n) {
-			var r = _.find(n.get('inc'),function(branchIn) {
-				return branchIn == this.get('key');
-			},this);
-			if (r != undefined) {
-				n.set('backbranch',this);
-				this.set('node',n);
-				this.get('node').linkByKey(branches,nodes);
-			}
-		},this);
-		return this;
-	}
-});
-window.BranchCollection = Backbone.Collection.extend({
-	model: Branch
-});
-window.Node = Backbone.Model.extend({
-	defaults: function() {
-		return {
-			branches: null,
-			backbranch: null, 
-			type: "",
-			inc: [],
-			out: []
-		}
-	},
-	initialize: function() {
-		this.set('branches',new BranchCollection);
-	},
-	branchByKey: function(key) {
-		var result = this.get('branches').find(function(b) { 
-			return b.get('key') == key; 
-		},this);
-
-		if (result != undefined)
-			return result;
-		this.get('branches').each(function(b) {
-			var r = b.get('node').branchByKey(key);
-			if (r != undefined) 
-				result = r;
-		},this);
-
-		if (result != undefined) 
-			return result;
-		return false;
-
-		
-	},
-
-
-	addBranch: function(b) {
-		if (!(b instanceof Branch))
-			b = new Branch({backnode: this,txt:b.txt,key:b.key});
-		else
-			b.set('backnode',this);
-
-		this.get('branches').add(b);
-	},
-
-	asJSON: function() {
-		var json = {branches: []};
-		this.get('branches').each(function(k){
-			json.branches.push(k.asJSON());
-		},this);
-		json.type = this.get('type');
-		return json;
-	},
-
-	treeDepth: function() {
-		var depths = _.map(this.get('branches').models,function(k){
-			return (k.get('node') != null) ? 1 + k.get('node').treeDepth() : 1;
-		},this);
-		if (depths.length == 0)
-			return 1;
-		return _.max(depths); 
-	},
-
-	linkByKey: function(branches,nodes) {
-		_.each(this.get('out'),function(o) {
-		
-			var key = _.find(branches,function(b) { 
-				return b.get('key') == o;
-			});
-			this.addBranch(key);
-			this.get('branches').last().linkByKey(branches,nodes);
-		
-		},this);
-		return this;
-	},
-
-	depth: function() { 
-		var d 	= 1,
-				bb 	= this.get('backbranch');
-
-
-		while(bb != null) {			
-			d++;
-			if (bb.get('backnode')) {
-//				d++;
-				if(bb.get('backnode').get('backbranch')){
-					bb = bb.get('backnode').get('backbranch')
-				}
-				else {
-					break;
-				}
-			}
-		}
-		return d;
-	}
-
-});
 
